262e6dbc7b19a5a75e76c7d210206a03f0795cf5
Jusayne Chambers <jusayne123@gmail.com>
2025-03-24 17:47:20 -0500

    Almost Complete

--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
-
+from app import mail 
+from flask_mail import Message
+from flask_mail import Message 
+from app.forms import ContactForm
 ###
 # Routing for your application.
 ###
@@ -16,6 +18,21 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    form = ContactForm()
+    if form.validate_on_submit() :
+         if request.method == "POST":
+            msg = Message(request.form['subject'],
+            sender = (request.form['name'], request.form['email']),
+            recipients=['to@example.com'])
+            msg.body = request.form['message']
+            mail.send(msg)
+            flash("Your email was sent successfully")
+            return redirect(url_for('home'))
+    return render_template('contact.html', form = form)
 
 
 ###
